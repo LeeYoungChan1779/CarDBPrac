b8512799c1f730bbce8436a3bafb05d7e43b4570
DESKTOP-KLHQH2Q\LCT <dldudcks159@gmail.com>
2018-01-31 14:32:05 +0900

    오류뜰껄

--- src/carDBPrac/main/CarDBPracMain.java
+++ src/carDBPrac/main/CarDBPracMain.java
@@ -1,10 +1,8 @@
 package carDBPrac.main;
 
 public class CarDBPracMain {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("작업 완료!!");
 
 	}
-
 }
